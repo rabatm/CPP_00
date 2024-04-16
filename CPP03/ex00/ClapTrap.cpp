5ca9906f04c112838de7f74c527509e5611766d1
RABAT Martin <martin.Rabat@gmail.Com>
2024-04-16 19:09:32 +0200

    start ex03

---
+++ CPP03/ex00/ClapTrap.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabat <mrabat@student.42perpignan.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 18:46:24 by mrabat            #+#    #+#             */
+/*   Updated: 2024/04/16 19:06:08 by mrabat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string myName) : _name(myName)
+{
+};
+
+ClapTrap::ClapTrap(ClapTrap const &src)
+{
+	this->_name = src._name;
+	this->_hitPoints = src._hitPoints;
+	this->_energyPoints = src._hitPoints;
+	this->_attackDamage = src._attackDamage;
+};
+
+ClapTrap::~ClapTrap(void)
+{
+
+};
+
+void ClapTrap::attack(ClapTrap &target)
+{
+	target.takeDamage(this->_hitPoints);
+}
+
+
---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabat <mrabat@student.42perpignan.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 18:41:59 by mrabat            #+#    #+#             */
+/*   Updated: 2024/04/16 19:06:03 by mrabat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+class ClapTrap
+{
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap const &src);
+		~ClapTrap(void);
+
+		ClapTrap &operator=(ClapTrap const &rhs);
+
+		void attack(ClapTrap &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+	protected:
+		std::string _name;
+		unsigned int _hitPoints;
+		unsigned int _energyPoints;
+		unsigned int _attackDamage;
+};
+
+#endif
