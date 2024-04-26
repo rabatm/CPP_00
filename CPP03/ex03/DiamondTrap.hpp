9c6aff55c7baae254817a50dc477269afe43670c
martin.Rabat@gmail.Com <martin.Rabat@gmail.Com>
2024-04-26 16:14:56 +0200

    2

--- CPP03/ex03/DiamondTrap.hpp
+++ CPP03/ex03/DiamondTrap.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
-class DiamondTrap : public ScavTrap
+class DiamondTrap : public ScavTrap, public FragTrap
 {
 	private:
 		std::string _name;
 	public:
 		DiamondTrap(std::string name);
 		DiamondTrap(DiamondTrap const &src);
-		~DiamondTrap();
+		virtual ~DiamondTrap();
 		DiamondTrap &operator=(DiamondTrap const &rhs);
 
 		void attack(ClapTrap &target);
